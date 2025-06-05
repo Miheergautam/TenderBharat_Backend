228f117cf89f87ff8741ad65d4477ad2744b7e9b
Miheer Gautam <miheergautam04@gmail.com>
2025-06-05 20:28:40 +0530

    feat: Add GET request to get tender by tender_id

--- app/controllers/tender_controller.py
+++ app/controllers/tender_controller.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException
 from fastapi import Request
+from bson import ObjectId
+
 
 async def get_all_tenders(request: Request):
     tenders_collection = request.app.mongodb["Tenders"]
@@ -11,5 +13,21 @@ async def get_all_tenders(request: Request):
             tender["zip_file_id"] = str(tender["zip_file_id"])
         tenders.append(tender)
     return tenders
+
+async def get_tender_by_id(request: Request, tender_id: str):
+    tenders_collection = request.app.mongodb["Tenders"]
+
+    if not ObjectId.is_valid(tender_id):
+        raise HTTPException(status_code=400, detail="Invalid Tender ID")
+
+    tender = await tenders_collection.find_one({"_id": ObjectId(tender_id)})
+    if not tender:
+        raise HTTPException(status_code=404, detail="Tender not found")
+
+    tender["_id"] = str(tender["_id"])
+    if tender.get("zip_file_id"):
+        tender["zip_file_id"] = str(tender["zip_file_id"])
+
+    return tender
 
 
--- app/routes/tender_routes.py
+++ app/routes/tender_routes.py
@@ -7,3 +7,7 @@ router = APIRouter()
 @router.get("/tenders")
 async def get_all_tenders(request: Request):
     return await tender_controller.get_all_tenders(request)
+
+@router.get("/tenders/{tender_id}")
+async def get_tender_by_id(tender_id: str, request: Request):
+    return await tender_controller.get_tender_by_id(request, tender_id)
